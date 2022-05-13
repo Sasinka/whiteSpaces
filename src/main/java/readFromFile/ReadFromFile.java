319acde1b08e9f5dbe74f97fc7bed4d807a2a704
rosec <roseckasara@gmail.com>
2022-05-13 10:22:44 +0200

    zapis dat

--- src/main/java/readFromFile/ReadFromFile.java
+++ src/main/java/readFromFile/ReadFromFile.java
@@ -5,20 +5,54 @@ import java.util.Scanner;
 
 public class ReadFromFile {
 
+    public static
+    void odstranMezery(String data){
+        data = data.replaceAll("\\s+", " ");
+        //char[] charArray = new char[data.length()];
+        //naplní charaArray
+        /*for(int i = 0; i < data.length(); i++){
+            charArray[i] = data.charAt(i);
+            if(Character.isWhitespace((char) charArray[i])) //... je bílý
+            {
+                 charArray[i] = " ";
+
+            }
+        }*/
+
+    }
 
     public static void nactiData() {
+
         try {
             File myObj = new File("../input.txt");
             Scanner myReader = new Scanner(myObj);
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
                 //tady něco udělat
+                odstranMezery(data);
                 System.out.println(data);
+                zapisData(data);
             }
             myReader.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+    }
+
+    public static void zapisData(String data){
+        try{
+            //File myObj = new File("../input.txt");
+            BufferedWriter writer = new BufferedWriter(new FileWriter("input1.txt"));
+            writer.write(data);
+            writer.close();
+        } catch (FileNotFoundException e) {
+        System.out.println("An error occurred.");
+        e.printStackTrace();
+
+    } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
